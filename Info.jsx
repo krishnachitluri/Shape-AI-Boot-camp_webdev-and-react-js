8546b2cd169dfa986f040eb212313db3fd1d4222
krishnachitluri <86249362+krishnachitluri@users.noreply.github.com>
2021-06-27 08:44:51 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javscript and react.js</h1>
+      <p> basic web dev React js boot camp</p>
+    </div>
+  );
+}
+
+export default Info;
